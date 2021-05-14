066893c6548ffc88548b8d2dc1dd6128832bdb52
Muhammad Ashir <mashir670@gmail.com>
2021-05-14 11:39:31 +0500

    added fps functionality through argparse

--- viz.py
+++ viz.py
@@ -1,18 +1,52 @@
-import sounddevice as sd
+# standard libs
 import asyncio
-import pygame as pg
-import sys
+import argparse
 
-size = (1920, 1080)
-threshold = int(sys.argv[1])
-fps = int(sys.argv[2])
+# third party libs
+import pygame as pg
+import sounddevice as sd
+
+
+# tried to learn "asyncio" while making this so called project. Still got roads to travel
+# a lot pygame functionality is still not implemented
+
+# argument parsing
+parser = argparse.ArgumentParser(description="uses monitor's backlight to visualize music")
+parser.add_argument("--fps", help="Frames per second of visuals (default=30)")
+parser.add_argument("--size", help="tuple for size of the screen ((1920,1080) by default)")
+parser.add_argument("--threshold", help="threshold for determining color (default=20000)")
+args = parser.parse_args()
+
+# defining some important variables
+if args.size is None:
+    size = (1920, 1080)
+else:
+    size = [tuple(int(x), int(y)) for x, y in (args.size).split(",")]
+if args.fps is None:
+    fps = 30
+else:
+    try:
+        fps = int(args.fps)
+    except Exception as e:
+        print(e)
+if args.threshold is None:
+    threshold = 20000
+else:
+    try:
+        threshold = int(args.threshold)
+    except Exception as e:
+        print(e)
+    
 
 # variable of sounddevice
-
 sd.default.samplerate = 22050
 block = sd.default.samplerate // fps
 
+
+# code for using input stream with async 
 async def inputstream_generator(block, channels=1, **kwargs):
+    # block: size of array which holds the input data 
+    # before putting it in a queue.
     q_in = asyncio.Queue()
     Loop = asyncio.get_event_loop()
 
@@ -28,7 +62,7 @@ async def inputstream_generator(block, channels=1, **kwargs):
             indata = await q_in.get()
             yield indata
 
-
+# determine color through a lot of if/else
 async def color_gen(threshold):
     async for indata in inputstream_generator(block):  
         color = (0,0,0)
@@ -64,15 +98,18 @@ async def color_gen(threshold):
             pass
         yield color
 
+# display the color on pygame window
 async def display(scr):
     async for color in color_gen(threshold):
         scr.fill(color)
         pg.display.flip()
 
+# main function obviously
 async def main():
     pg.init()
     scr = pg.display.set_mode(size)
     await asyncio.gather(display(scr))
 
+# running the main coroutine
 asyncio.run(main())
 
